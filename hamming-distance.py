8623e04edb4809d8e278af3e7fcce00a8bb4f8cd
Kelly <55807683+kellymhli@users.noreply.github.com>
2019-12-23 18:47:46 -0800

    Add input capability

--- hamming-distance.py
+++ hamming-distance.py
@@ -46,3 +46,8 @@ if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
         print('ALL TESTS PASSED')
+
+    sol = Solution()
+    x = int(input('x: '))
+    y = int(input('y: '))
+    print (sol.hammingDistance(x, y))
