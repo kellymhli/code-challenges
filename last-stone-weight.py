81977adb3312d2299fa2e0c5eda31e609479c9ba
Kelly <55807683+kellymhli@users.noreply.github.com>
2020-04-13 08:34:27 -0700

    Write last stone weight algo

---
+++ last-stone-weight.py
@@ -0,0 +1,15 @@
+def last_stone_weight(stones):
+    stones.sort()
+
+    while len(stones) > 1:
+        stones.sort()
+        if stones[-2] == stones[-1]:
+            stones = stones[:-2]
+        else:
+            stones = stones[:-2] + [stones[-1] - stones[-2]]
+
+    if not stones:
+        return 0
+    return stones[0]
+
+print(last_stone_weight([2,7,4,1,8,1]))
