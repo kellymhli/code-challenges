84d32d743aa1976a69a997b21187b693680725b3
Kelly <55807683+kellymhli@users.noreply.github.com>
2020-04-15 18:31:19 -0700

    Write word pattern algo

---
+++ word-pattern.py
@@ -0,0 +1,15 @@
+def get_pattern(iterable) -> list:
+    d, res = {}, []
+    for i, item in enumerate(iterable):
+        if item not in d:
+            d[item] = i
+        res += [d[item]]
+    return res
+
+def word_pattern(pattern, string) -> bool:
+    """Determine if string follows the pattern."""
+    string = string.split()
+    return get_pattern(pattern) == get_pattern(string)
+
+print(word_pattern('abba', 'dog cat cat dog'))  # True
+print(word_pattern('abba', 'cat dog cat dog'))  # False
