a4cd8a5052762279540baba28d963f6b0d853669
Kelly <55807683+kellymhli@users.noreply.github.com>
2020-05-04 11:57:34 -0700

    Write destination city algo

---
+++ destination-city.py
@@ -0,0 +1,13 @@
+def destCity(paths) -> str:
+    d = {}
+    for a, b in paths:
+        d[a] = b
+
+    for a, b in paths:
+        path = a
+        while path in d:
+            path = d[path]
+        return path
+
+print(destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]))  # San Pablo
+print(destCity([["B","C"],["D","B"],["C","A"]]))  # A
