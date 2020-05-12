c70e0a716a254972d533f1348737d16678153046
Kelly <55807683+kellymhli@users.noreply.github.com>
2020-05-12 10:48:01 -0700

    Add docstring and test case

--- song-durations-divisible-by-60.py
+++ song-durations-divisible-by-60.py
@@ -1,6 +1,9 @@
 def num_pairs_divisible_by_60(times) -> int:
+    """Return number of pairs of songs have a total duration divisible by 60."""
+
     d = {}
     res = 0
+
     for i in range(len(times)):
         remainder = times[i] % 60
         if 60 - remainder in d:
@@ -8,8 +11,10 @@ def num_pairs_divisible_by_60(times) -> int:
         elif remainder == 0:
             res += d.get(remainder, 0)
         d[remainder] = d.get(remainder, 0) + 1
+
     return res
 
 print(num_pairs_divisible_by_60([30,20,150,100,40]))  # 3
 print(num_pairs_divisible_by_60([60,60,60]))  # 3
 print(num_pairs_divisible_by_60([]))  # 0
+print(num_pairs_divisible_by_60([111,33,129,87,88,92]))  # 3
